0662fe26f9a3dfc69cce37c9c1f22872f009c5c9
Chris Wong <longr2512@gmail.com>
2026-08-31 21:53:12 -0700

    fix(exec): close retry release manifest

--- control_plane/chairman_control_room_remote.py
+++ control_plane/chairman_control_room_remote.py
@@ -54,6 +54,7 @@ REQUIRED_RUNTIME_PATHS = frozenset({
     "control_plane/executive_inbox.py",
     "control_plane/executive_orchestration_principal.py",
     "control_plane/executive_orchestration_result.py",
+    "control_plane/executive_retry_safety.py",
     "control_plane/executive_runtime.py",
     "control_plane/executive_supervisor.py",
     "control_plane/executive_worker_broker.py",
